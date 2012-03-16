580df4b7acce768b68d20d1aefd8f62995447d71
pschaus <devnull@localhost>
2012-03-16 16:32:56 +0100

    scampi visualization and openstreetmap connector

--- build.sbt
+++ build.sbt
@@ -5,7 +5,7 @@ version := "1.0"
 
 organization := "nside"
 
-scalaVersion := "2.9.1"
+scalaVersion := "2.9.1-1"
 
 autoCompilerPlugins := true
 
@@ -27,6 +27,13 @@ libraryDependencies += "org.scalatest" % "scalatest" % "1.4.RC2"
 
 
 libraryDependencies += "com.novocode" % "junit-interface" % "0.7" % "test->default"
+
+resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+
+ 
+
+libraryDependencies += "com.typesafe.akka" % "akka-actor" % "2.0"
+
 
 
 testListeners <<= target.map(t => Seq(new eu.henkelmann.sbt.JUnitXmlTestsListener(t.getAbsolutePath)))
---
+++ src/main/java/scampi/visual/MapLine.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *  
+ * Contributors:
+ *      www.n-side.com
+ ******************************************************************************/
+
+package scampi.visual;
+
+
+/**
+ * @author Pierre Schaus
+ */
+public class MapLine {
+	double lt1, lg1, lt2, lg2;
+	Map map;
+
+	public MapLine(Map map,double lt1, double lg1, double lt2, double lg2) {
+		this.map = map;
+		this.lt1 = lt1;
+		this.lg1 = lg1;
+		this.lt2 = lt2;
+		this.lg2 = lg2;
+	}
+
+	public void setDest(double lt2, double lg2) {
+		this.lt2 = lt2;
+		this.lg2 = lg2;
+		map.viewer.repaint();
+
+	}
+
+	public void setOrig(double lt1, double lg1) {
+		this.lt1 = lt1;
+		this.lg1 = lg1;
+		map.viewer.repaint();
+	}		
+}
