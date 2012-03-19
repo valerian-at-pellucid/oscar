4053f4afce29d6fec40fc289e87e69e1810ea6fd
VictorJavAdore <devnull@localhost>
2012-03-19 13:07:30 +0100

    Added comments to Cubes.scala (cubes' problem).

--- src/main/scala/scampi/cp/examples/Cubes.scala
+++ src/main/scala/scampi/cp/examples/Cubes.scala
@@ -2,6 +2,14 @@ package scampi.cp.examples
 
 import scampi.cp.modeling._
 import scampi.search._
+import collection.immutable.SortedSet
+
+/*
+ * Problem statement :
+ * There are 13 words : BUOY, CAVE, CELT, FLUB, FORK, HEMP, JUDY, JUNK, LIMN, QUIP, SWAG, VISA, WISH
+ * 24 different letters that appear in the 13 words.
+ * Assign the 24 letters appearing to 4 different cubes (one letter/face) so that the 4 letters of each word appears on different cubes.
+ */
 
 object Cubes extends CPModel {
 
@@ -11,36 +19,33 @@ object Cubes extends CPModel {
     
     val numCubes = 4
     val numFaces = 6
-    val numLetters = 24
     
     val words = "BUOY, CAVE, CELT, FLUB, FORK, HEMP, JUDY, JUNK, LIMN, QUIP, SWAG, VISA, WISH".split(", ")
-    val intToLetter = for(i <- "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,Y".split(",")) yield i.charAt(0)
-    def letterToInt(letter: Char): Int = intToLetter.indexOf(letter)
+    val letters = words.foldLeft(SortedSet.empty[Char]){_++_}.toSeq // Set of all 24 letters
+    val numLetters = letters.size
+    def letterToInt(letter: Char): Int = letters.indexOf(letter) // Letter from letter index
     
-    var placement = for(i <- 0 until numLetters) yield CPVarInt(cp, 0 until numCubes)
+    val placement = for(i <- 0 until numLetters) yield CPVarInt(cp, 0 until numCubes) // The cube (0 to 3) on which each letter is placed
     
-    var nbSol = 0
-    
-    cp.solve subjectTo
+    cp.solve() subjectTo
     {
-      cp.add(gcc(placement, 0 until numCubes, numFaces, numFaces), Strong)
-      for(word <- words) cp.add(alldifferent(for(letter <- word.toCharArray()) yield placement(letterToInt(letter))), Strong)
-    } exploration {
+      cp.add(gcc(placement, 0 until numCubes, numFaces, numFaces), Strong) // There must be exactly 6 letters on each cube
+      for(word <- words)
+        cp.add(alldifferent( // The 4 letters of each word must be placed on different cubes
+            for(letter <- word.toCharArray()) yield placement(letterToInt(letter))
+        ), Strong)
+    } exploration { // Each letter will be assigned different cubes during the search
       loop(0 until numLetters) {
         l =>  cp.branchAll(0 until numCubes)(v => cp.post(placement(l) == v))
       }
-      nbSol += 1
       
-      for (cube <- 0 until numCubes) {
-        println("Cube "+cube+" : "+(for(i <- 0 until numLetters) yield {
-          if(placement(i).getValue() == cube) intToLetter(i)
-          else "."
-          }).mkString(" ")
-        )
+      for (cube <- 0 until numCubes) { // Printing the letters placed on each cube
+        println("Cube "+cube+" : "+
+            placement.zipWithIndex.map {
+              case (v,i) => if (v.getValue()==cube) letters(i) else "."}.mkString(" "))
       }
     }
     
-    println("#sol:"+nbSol)
     cp.printStats()
   }
 }
