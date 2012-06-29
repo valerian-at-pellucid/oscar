97a343034495e0559129d27f5ca583b9dad252af
admpsc <devnull@localhost>
2012-06-29 17:21:43 +0200

    fix header problem, include new header

--- src/main/scala/oscar/cbls/invariants/lib/logic/Accesses.scala
+++ src/main/scala/oscar/cbls/invariants/lib/logic/Accesses.scala
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License along with OscaR.
  * If not, see http://www.gnu.org/licenses/gpl-3.0.html
  ******************************************************************************/
- ******************************************************************************/
+
 /*
  * Copyright CETIC 2012 www.cetic.be
  *
