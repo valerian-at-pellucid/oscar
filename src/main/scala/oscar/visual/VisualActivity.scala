46ad5a8684eba6e12533401d606a70411cc3d028
rhartert <devnull@localhost>
2012-07-04 12:35:33 +0200

    - Update : Visual Activity and Visual Profile

--- src/main/scala/oscar/visual/VisualActivity.scala
+++ src/main/scala/oscar/visual/VisualActivity.scala
@@ -1,5 +1,28 @@
 package oscar.visual
 
-class VisualActivity {
-
+class VisualActivity(start : Int, end : Int, height : Int, color : Int) {
+  
+	def setStart(start : Int) {
+		
+	}
+	
+	def getStart() = start
+  
+	def setEnd(end : Int) {
+		
+	}
+	
+	def getEnd() = end
+  
+	def setHeight(height : Int) {
+		
+	}
+	
+	def getHeight() = height
+  
+	def setColor(color : Int) {
+		
+	}
+	
+	def getColor() = color
 }
--- src/main/scala/oscar/visual/VisualProfile.scala
+++ src/main/scala/oscar/visual/VisualProfile.scala
@@ -1,5 +1,66 @@
 package oscar.visual
 
-class VisualProfile {
+import oscar.visual._
+import java.awt.geom.Line2D;
 
+class VisualProfile(b : Boolean) extends VisualDrawing(b : Boolean) {
+
+	private var activities : List[VisualActivity] = Nil
+	
+	def addActivity(activity : VisualActivity) { activities = activity :: activities }
+	def addActivities(activities : List[VisualActivity]) { this.activities = activities }
+	def resetActivities() { activities = Nil }
+	
+	def update {
+		
+		var points : List[Tuple2[Int, Int]] = Nil
+		
+		for (act <- activities) {
+			points = (act.getStart, act.getHeight) :: points
+			points = (act.getEnd, -act.getHeight) :: points
+		}
+		
+		// Sort the points
+		val sortedPoints = points.sort((s, t) => s._1 < t._1)
+		
+		// First point
+		var lastPoint = (0,0)
+		var nextPoint = (0,0)
+		
+		for (point <- sortedPoints) {
+			
+			if (nextPoint._1 == point._1) {
+				//Edit the height of the next point to draw
+				nextPoint = (point._1, nextPoint._2 + point._2)	
+				
+			} else {
+				
+				new ColoredShape[Line2D](this, new Line2D.Double(lastPoint._1, lastPoint._2, nextPoint._1, nextPoint._2))
+				new ColoredShape[Line2D](this, new Line2D.Double(nextPoint._1, nextPoint._2, point._1, nextPoint._2))
+				
+				lastPoint = (point._1, nextPoint._2)
+				nextPoint = (point._1, nextPoint._2 + point._2)
+				
+			}
+		}
+		
+		new ColoredShape[Line2D](this, new Line2D.Double(lastPoint._1, lastPoint._2, nextPoint._1, nextPoint._2))
+	}
+	
+	def main(args: Array[String]) {
+							  
+		val frame = new VisualFrame("toto")						  
+		val profile = new VisualProfile(false)
+		val inf = frame.createFrame("Ressource Consumption")
+		inf.add(profile)
+		frame.pack
+		
+		val activities = List(new VisualActivity(1, 4, 1, 0),
+							  new VisualActivity(2, 3, 1, 0),
+							  new VisualActivity(2, 4, 2, 0),
+							  new VisualActivity(5, 6, 1, 0))
+		
+		profile.addActivities(activities)
+		profile.update
+	}
 }
