c0ac78c73a3686a25dd310ddabc0c8b4a5102fe8
rhartert <devnull@localhost>
2012-08-24 11:01:52 +0200

    fixes #20

---
+++ src/test/scala/oscar/cp/test/TestCPVarInt.scala
@@ -0,0 +1,28 @@
+package oscar.cp.test
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+
+import oscar.cp.core._
+import oscar.cp.modeling._
+
+class TestCPVarInt extends FunSuite with ShouldMatchers {
+
+	test("Test1 : Median") {
+
+		val cp = CPSolver()
+		
+		val a = CPVarInt(cp, Array(0, 1, 2, 3, 4))
+		
+		a.median should be(2)
+		
+		a.removeValue(1)
+		
+		a.median should be(3)
+		
+		a.removeValue(4)
+		
+		a.median should be(2)
+	}
+
+}
