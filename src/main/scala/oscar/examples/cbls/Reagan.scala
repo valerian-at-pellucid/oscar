799062c279b6379e1854771849b4278e8f52cd4f
Renaud De Landtsheer <renaud.delandtsheer@cetic.be>
2012-10-08 16:35:44 +0200

    solves #33

--- src/main/scala/oscar/examples/cbls/Reagan.scala
+++ src/main/scala/oscar/examples/cbls/Reagan.scala
@@ -56,8 +56,8 @@ object Reagan extends App {
   val Boire = Task(3, planning, "drink")
   Boire uses 3 ofResource Reagan
 
-//  val Digerer = SuperTask(Manger, Dormir, "digest")
-//  Digerer uses 1 ofResource Reagan
+  val Digerer = SuperTask(Manger, Dormir, "digest")
+  Digerer uses 1 ofResource Reagan
 
   Reflechir precedes Boire
   Manger precedes Dormir
@@ -66,7 +66,7 @@ object Reagan extends App {
   planning.close()
   model.close(false)
  // println(model.dumpToDot(true, true))
-  solver.Solve(10, 4, solver.WorseFirst(), 2, 50)
+  solver.Solve(15, 10, solver.WorseFirst(), 2, 50)
 
   println(planning.toAsciiArt)
 }
