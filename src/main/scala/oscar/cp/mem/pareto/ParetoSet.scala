961cbdd4ef4b8339d7afa3e4e8480112a7ab07d3
rhartert <devnull@localhost>
2012-10-14 20:37:15 +0200

    Progress...

--- src/main/scala/oscar/cp/mem/pareto/ParetoSet.scala
+++ src/main/scala/oscar/cp/mem/pareto/ParetoSet.scala
@@ -1,7 +1,5 @@
 package oscar.cp.mem.pareto
 
-import oscar.cp.mem.Hypervolume
-
 class ParetoSet[S](n : Int) extends Pareto[S](n) {
 	
 	override def currentPoint : ParetoPoint[S] = {
@@ -26,22 +24,7 @@ class ParetoSet[S](n : Int) extends Pareto[S](n) {
 		else set.dequeue()
 	}
 	
-	override def hypervolume(ref : Array[Int]) : Double = {
-	
-		val points : Array[Array[Double]] = paretoToArray
-		
-		/*for (i <- 0 until points.size) {
-			points(i)(0) = ref(0) - points(i)(0)
-			points(i)(1) = ref(1) - points(i)(1)
-		}*/
-		
-		val h : Double = Hypervolume.simple2DHypervolume(points, Array(0, 0)) / (ref(0) * ref(1))	
-		h
-	}
-	
-	private def paretoToArray = set.map(p => p.toArray).toArray
-	
-	override def toString : String = "{" + set.mkString(", ") + "}"
+	override def hypervolume(ref : Array[Int]) : Double = 0
 }
 
 object ParetoSet {
