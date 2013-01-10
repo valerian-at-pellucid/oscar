049d818c676e92b9a4d2e33c8374b0ab49b88822
pschaus <devnull@localhost>
2013-01-10 15:41:48 +0100

    perf tool, add scala bin directory as an instance variable for jenkins

--- perf.sh
+++ perf.sh
@@ -7,11 +7,12 @@ rm -f *.class
 D1=$(date +"%m-%d-%y")
 D2=$(date +%s)
 C=`hg id -i`
+echo "scala10 directory $SCALA"
 for f in `ls -1 *.scala`; do
   echo "File -> $f"
   f2=${f%%??????}
-  scalac  -cp oscar.jar  -P:continuations:enable $f
-  SECONDS=0; scala  -cp oscar.jar  -P:continuations:enable $f2 ; echo "that took approximately $SECONDS seconds"
+  $SCALA10/scalac  -cp oscar.jar  -P:continuations:enable $f
+  SECONDS=0; $SCALA/scala  -cp oscar.jar  -P:continuations:enable $f2 ; echo "that took approximately $SECONDS seconds"
   echo $f $SECONDS $D2 $D1 $C >> ../perfresults.txt
   echo $f $SECONDS $D $C
 done
