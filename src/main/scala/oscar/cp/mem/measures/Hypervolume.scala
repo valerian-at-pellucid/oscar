a93208de0a304c33b4c8c5d1c5c6f5e0181715d9
rhartert <devnull@localhost>
2013-01-14 10:50:02 +0100

    Fix overflow in Hypervolume computation

--- src/main/scala/oscar/cp/mem/measures/Hypervolume.scala
+++ src/main/scala/oscar/cp/mem/measures/Hypervolume.scala
@@ -2,37 +2,36 @@ package oscar.cp.mem.measures
 
 object Hypervolume {
 
-  def hypervolumeNadir(set: Array[(Int, Int)], nadir: (Int, Int), V: (Double, Double)): Double = {
+  def hypervolumeNadir(set: Array[(Int, Int)], nadir: (Int, Int)): Double = {
     var prevObj2 = nadir._2
     var volume = 0.0
-    for (i <- 0 until set.size) {
-      val dObj2 = prevObj2 - set(i)._2
-      val dObj1 = nadir._1 - set(i)._1
-      prevObj2 = set(i)._2
-      volume += ((dObj2 * dObj1))
+    for ((obj1, obj2) <- set) {
+      val dObj2 = prevObj2 - obj2
+      val dObj1 = nadir._1 - obj1
+      prevObj2 = obj2
+      val v = (dObj2 * dObj1).toDouble / 100000000
+      if (v > 0) volume += v
     }
     volume
   }
-  
-  def join(set : Array[(Int, Int)], ref : Array[(Int, Int)]): Array[(Int, Int)] = {  
-    var joinSet = set
-    for (r <- ref) joinSet = joinSet.filter(s => s._1 >= r._1 && s._2 >= r._2)
-    joinSet = joinSet union ref
-    return joinSet
-  }
 
   def main(args: Array[String]) {
-    
-    val rhSet1 = (SetParser.parseSet("setPointAB10Bunch.txt")).sortBy(_._1)  
-    val rhSet2 = (SetParser.parseSet("setPointAB15c2.txt")).sortBy(_._1) 
-    val rhSet3 = (SetParser.parseSet("setPointAB15c3.txt")).sortBy(_._1) 
-    
-    val nadir = (180000, 180000)
-    val V = (180000.0, 180000.0)
 
-    println("rh\t : " + hypervolumeNadir(rhSet1, nadir, V)/100000000)   
-    println("rh\t : " + hypervolumeNadir(rhSet2, nadir, V)/100000000)
-    println("rh\t : " + hypervolumeNadir(rhSet3, nadir, V)/100000000)
+    val rhSet1 = (SetParser.parseSet("setPointABtest.txt")).sortBy(_._1)
+    val rhSet2 = (SetParser.parseSet("setPointACtest.txt")).sortBy(_._1)
+    val rhSet3 = (SetParser.parseSet("setPointADtest.txt")).sortBy(_._1)
+    val rhSet4 = (SetParser.parseSet("setPointBCtest.txt")).sortBy(_._1)
+    val rhSet5 = (SetParser.parseSet("setPointBDtest.txt")).sortBy(_._1)
+    val rhSet6 = (SetParser.parseSet("setPointCDtest.txt")).sortBy(_._1)
+
+    val nadir = (180000, 180000)
+
+    println("rh\t : " + hypervolumeNadir(rhSet1, nadir))
+    println("rh\t : " + hypervolumeNadir(rhSet2, nadir))
+    println("rh\t : " + hypervolumeNadir(rhSet3, nadir))
+    println("rh\t : " + hypervolumeNadir(rhSet4, nadir))
+    println("rh\t : " + hypervolumeNadir(rhSet5, nadir))
+    println("rh\t : " + hypervolumeNadir(rhSet6, nadir))
   }
 }
 
