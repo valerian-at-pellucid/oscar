46bdf2483ca2b2241176d0a3fc61ab9cf7151eaf
rhartert <devnull@localhost>
2013-02-11 17:39:18 +0100

    Add some instances for the bTSP

---
+++ src/main/scala/oscar/des/engine/Event.scala
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * This file is part of OscaR (Scala in OR).
+ *  
+ * OscaR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ * 
+ * OscaR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with OscaR.
+ * If not, see http://www.gnu.org/licenses/gpl-3.0.html
+ ******************************************************************************/
+
+package oscar.des.engine
+
+/**
+ * Objects stored in the main queue of the simulation. The modeler should not have knowledge of it.
+ * @author Pierre Schaus, Sebastien Mouthuy
+ */
+abstract class SimEvent[A<%Ordered[A]](val time: A) extends Ordered[SimEvent[A]] {
+	
+	def compare(that : SimEvent[A]) = time.compare(that.time)
+	
+	
+}
+
+class WaitEvent[A<%Ordered[A]](time: A, block: A => Boolean ) extends SimEvent[A](time) {
+	
+	def process(){
+	  block(time)
+	}
+	
+}
+
+
+
