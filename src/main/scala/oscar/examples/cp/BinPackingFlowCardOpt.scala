981c86adef152057f825e18c694b6b8c4cb868c0
Fran?ois Pelsser <oscar@frol.be>
2013-02-20 17:39:42 +0100

    tester

--- src/main/scala/oscar/examples/cp/BinPackingFlowCardOpt.scala
+++ src/main/scala/oscar/examples/cp/BinPackingFlowCardOpt.scala
@@ -6,6 +6,7 @@ import oscar.search._
 import oscar.cp.core._
 import oscar.cp.constraints.BinPackingFlow
 import oscar.cp.constraints.BinPackingFlowExtended
+import java.text.DecimalFormat
 
 case class BinPackingInstance()
 {
@@ -33,25 +34,74 @@ class BinPackingTester(bpi:BinPackingInstance)
 	  
 	  bpf.c
 	}
-	def testNormalVsExtended() = 
+	
+	/*
+	 * @return
+	 * countNorm, countExt, normFail, extFail
+	 */
+	def testNormalVsExtended(): (Int,Int, Int, Int) =
 	{
+	  var normalFail = false
+	  var extendedFail = false
+	  var normalPropagateRestult = Array[CPVarInt]()
+	  var extendedPropagateRestult = Array[CPVarInt]()
+	  
 	  try{
-		  print("result normal \n" + solve(false).mkString("\n") + "\n")
+		  normalPropagateRestult = solve(false)
 	  } 
 	  catch {
-	    case e:NoSolutionException => printf("normal fail \n") 
+	    case e:NoSolutionException => normalFail = true
 	    
 	  }
 	  
 	   try{
-		  print("result extended  \n" + solve(true).mkString("\n") + "\n")
+		  extendedPropagateRestult = solve(true)
 	  } 
 	  catch {
-	    case e:NoSolutionException => printf("extended fail \n") 
+	    case e:NoSolutionException => extendedFail = true 
 	    
+	  }
+	  
+	  if (normalFail && !extendedFail)
+	    throw new Exception("Normal find failure but not extended")
+	  
+	  if(normalFail) (0,0,1,1) 
+ 	  else if (extendedFail) (0,0,0,1)
+	  else{
+		  val resultDiff = normalPropagateRestult.zip(extendedPropagateRestult).foldLeft((0,0)){
+		    case ((countNorm, countExt), (n, e)) if n.getMin > e.getMin || n.getMax < e.getMax=> 
+		      throw new Exception("Normal did better than extended")
+		    case ((countNorm, countExt), (n, e)) => 
+		      (countNorm + n.getSize,countExt + e.getSize)
+		  }
+		  (resultDiff._1,resultDiff._2,0,0) 
 	  }
 	   
 	}
+}
+
+object BinPackingTester{
+  	def testAndStats(instances : Seq[BinPackingInstance]) = {
+	  var extBetter	= 0
+	  var extBetterNoFail	= 0
+	  var normFail 	= 0
+	  var extFail 	= 0
+	  var extImprovementMeanCount = 0.0
+	  
+	  for (inst <- instances) new BinPackingTester(inst).testNormalVsExtended match
+	  {
+	    case (_,_,1,1) => normFail +=1; extFail +=1
+	    case (_,_,_,1) => extFail +=1; extBetter
+	    case (n,e,_,_) if (n!=e) => extBetter += 1;extBetterNoFail += 1;;  extImprovementMeanCount += (e + 0.0)/n
+	    
+	  }
+	  
+	  val formatter = new DecimalFormat("#.###")
+	  print("Extended is better in " + formatter.format(((extBetter + 0.0) / instances.size)*100) + "% cases,"
+	      +" it found " + extFail + " fails against " + normFail +" for the normal version"
+	      +" and the improvement mean is " + formatter.format(extImprovementMeanCount / extBetterNoFail) + " by cardinality\n");
+	}
+  
 }
 
 object BinPackingFlowCardOpt extends App {
@@ -124,10 +174,7 @@ object BinPackingFlowCardOpt extends App {
   }
 	  
   
-  val bpt = new BinPackingTester(test3)
-  
-  bpt.testNormalVsExtended()
-  
+  BinPackingTester.testAndStats(List(test1,test2,test3))
  
   
   
