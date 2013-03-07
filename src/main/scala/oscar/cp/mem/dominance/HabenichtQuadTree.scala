0b6e3176f2549ca50150bda99428fee26926616d
rhartert <devnull@localhost>
2013-03-07 22:25:32 +0100

    Fix some bug in Habenicht

--- src/main/scala/oscar/cp/mem/dominance/HabenichtQuadTree.scala
+++ src/main/scala/oscar/cp/mem/dominance/HabenichtQuadTree.scala
@@ -12,87 +12,71 @@ class HabenichtQuadTree[V](private val nDim: Int) {
   private val Quadrants = 0 until nQuadrants
   private val usedQuadrants = 1 until nQuadrants - 1
 
-  private val bestQuad = Quadrants.min
-  private val worstQuad = Quadrants.max
+  private val bestQuad = Quadrants.max
+  private val worstQuad = Quadrants.min
 
-  private var root: QuadNode = Empty
+  private var root: Option[QuadNode] = None
 
   // Data structure
   // --------------
 
-  abstract class QuadNode {
-    def isEmpty: Boolean
-    def child(quad: Int): QuadNode
-    def hasChild(quad: Int): Boolean
-    def insertQuad(k: Array[Int]): Int
-  }
+  case class QuadNode(keys: Array[Int], value: V) {
 
-  case object Empty extends QuadNode {
-    def isEmpty = true
-    def child(quad: Int): QuadNode = throw new NoSuchElementException("Empty node")
-    def hasChild(quad: Int): Boolean = false
-    def insertQuad(k: Array[Int]): Int = throw new NoSuchElementException("Empty node")
-  }
-
-  case class NonEmpty(keys: Array[Int], value: V) extends QuadNode {
-
-    private[HabenichtQuadTree] val children: Array[QuadNode] = Array.fill(nQuadrants - 2)(Empty)
-    private[HabenichtQuadTree] var father: QuadNode = Empty
+    private[HabenichtQuadTree] val children: Array[Option[QuadNode]] = Array.fill(nQuadrants - 1)(None)   
+    def child(quad: Int) = children(quad).get
+    def hasChild(quad: Int) = !children(quad).isEmpty
+    
+    private[HabenichtQuadTree] var _father: Option[QuadNode] = None
+    def hasFather = _father.isDefined
+    def father = _father.get
+    def father_= (node: QuadNode) { _father = Some(node)}
+    
     private[HabenichtQuadTree] var quad: Int = bestQuad
 
-    def isEmpty = false
-    def child(quad: Int) = children(quad)
-    def hasChild(quad: Int) = !children(quad).isEmpty
-
     // Returns the id of the relevant quadrant of the QuadTree (pareto dominance based)
-    def insertQuad(k: Array[Int]): Int = insertQuad0(k, 0, 0, false, false)
+    def insertQuad(node: QuadNode): Int = insertQuad0(node.keys, 0, 0, false, false)
     private def insertQuad0(k: Array[Int], dim: Int, quadrant: Int, dom: Boolean, notDom: Boolean): Int = {
       if (dim == nDim) if (dom && !notDom) bestQuad else quadrant
-      else if (k(dim) < keys(dim)) insertQuad0(k, dim + 1, quadrant << 1, true, notDom)
-      else if (k(dim) == keys(dim)) insertQuad0(k, dim + 1, (quadrant << 1) + 1, dom, notDom)
-      else insertQuad0(k, dim + 1, (quadrant << 1) + 1, dom, true)
+      else if (k(dim) > keys(dim)) insertQuad0(k, dim + 1, (quadrant << 1) + 1, true, notDom)
+      else if (k(dim) == keys(dim)) insertQuad0(k, dim + 1, quadrant << 1, dom, notDom)
+      else insertQuad0(k, dim + 1, quadrant << 1, dom, true)
     }
 
-    override def toString: String = "NonEmpty(keys: " + keys.mkString(",") + ", value: " + value + ")"
+    override def toString: String = "QuadNode(keys: " + keys.mkString(",") + ", value: " + value + ")"
   }
 
   // Insertion
   // ---------
 
   def insert(keys: Array[Int], value: V) {
-    val newNode = NonEmpty(keys, value)
-    if (root.isEmpty) root = newNode
-    else process(newNode, toNonEmpty(root)) // Safe cast
+    val newNode = QuadNode(keys, value)
+    if (!root.isDefined) root = Some(newNode)
+    else process(newNode, root.get) // Safe cast
   }
   
   // Process
   // -------
 
-  def process(newNode: NonEmpty, root: NonEmpty) {
+  def process(newNode: QuadNode, root: QuadNode) {
     
-    val quad = root.insertQuad(newNode.keys)
+    val quad = root insertQuad newNode
     
-    if (quad == bestQuad) replace(newNode, root)
-    else if (quad != worstQuad){     
-      // Test 2
-      for (q <- asGood(quad) if q != quad) {
-        if (root.hasChild(q)) {
-          val child = toNonEmpty(root.child(q)) // Safe cast
-          if (checkDominance(newNode, child)) return
-        }
-      }    
+    if (quad == worstQuad) {}
+    
+    else if (quad == bestQuad) replace(newNode, root)
+    
+    else if (!isDominated(newNode, root, better(quad))) {     
+      
       // Test 1
-      for (q <- asBad(quad) if q != quad) {
-        if (root.hasChild(q)) {
-          val child = toNonEmpty(root.child(q)) // Safe cast
-          root.children(q) = filterDominance(newNode, child)
-        }
+      for (q <- worst(quad) if root hasChild q) {
+        root.children(q) = filterDominance(newNode, root.child(q)) // Safe
       }
+      
       // Keep processing
-      if (root.hasChild(quad)) process(newNode, toNonEmpty(root.child(quad))) // Safe cast
+      if (root.hasChild(quad)) process(newNode, root.child(quad)) // Safe
       // Insertion
       else {
-        root.children(quad) = newNode
+        root.children(quad) = Some(newNode)
         newNode.quad = quad
         newNode.father = root
       }
@@ -102,94 +86,85 @@ class HabenichtQuadTree[V](private val nDim: Int) {
   // Test 2
   // ------
 
-  def checkDominance(newNode: NonEmpty, node: QuadNode): Boolean = {
-    if (node.isEmpty) false
-    else {
-      val quad = node.insertQuad(newNode.keys)
-      if (quad == worstQuad) true
-      else checkDominance(asGood(quad), newNode, node)
-    }
+  def isDominated(newNode: QuadNode, node: QuadNode, quads: IndexedSeq[Int]): Boolean = {
+    if (quads.isEmpty) false
+    else if (!node.hasChild(quads.head)) isDominated(newNode, node, quads.tail)
+    else if (checkDominance(newNode, node.child(quads.head))) true // Safe
+    else isDominated(newNode, node, quads.tail)
   }
 
-  def checkDominance(quads: IndexedSeq[Int], newNode: NonEmpty, node: QuadNode): Boolean = {
-    if (quads.isEmpty) false
-    else if (checkDominance(newNode, node.child(quads.head))) true
-    else checkDominance(quads.tail, newNode, node)
+  def checkDominance(newNode: QuadNode, node: QuadNode): Boolean = {
+    val quad = node insertQuad newNode
+    if (quad == worstQuad) true
+    else isDominated(newNode, node, asGood(quad))
   }
 
   // Test 1
   // ------
 
-  def filterDominance(newNode: NonEmpty, root: NonEmpty): QuadNode = {
+  def filterDominance(newNode: QuadNode, root: QuadNode): Option[QuadNode] = {
 
-    val quad = newNode.insertQuad(root.keys)
+    val quad = newNode insertQuad root
     
     if (quad == worstQuad) {
       val newRoot = delete(root) 
-      if (!newRoot.isEmpty) filterDominance(newNode, toNonEmpty(newRoot)) // Safe cast
-      else Empty      
+      if (newRoot.isDefined) filterDominance(newNode, newRoot.get) // Safe
+      else None      
     }
     else {
-      for (q <- asBad(quad)) {
-        if (root.hasChild(q)) {
-          val child = toNonEmpty(root.child(q)) // Safe cast
-          filterDominance(newNode, child)
-        }
+      for (q <- asBad(quad) if root hasChild q) {
+        filterDominance(newNode, root.child(q)) // Safe
       }
-      root
+      Some(root)
     }
   }
   
   // Replace
   // -------
   
-  def replace(newNode: NonEmpty, oldRoot: NonEmpty) {
-    
+  def replace(newNode: QuadNode, oldRoot: QuadNode) {   
     move(newNode, oldRoot)
-    for (q <- usedQuadrants) {
-      if (oldRoot.hasChild(q)) {
-        val child = toNonEmpty(oldRoot.child(q)) // Safe cast
-        reconsider(child, newNode)
-      }
+    for (q <- usedQuadrants if oldRoot hasChild q) {
+      reconsider(oldRoot.child(q), newNode)
     }
   }
 
   // Reconsider
   // ----------
 
-  private def reconsider(node: NonEmpty, newRoot: NonEmpty) {  
+  private def reconsider(node: QuadNode, newRoot: QuadNode) {     
+    //Detach
+    if (node.hasFather) node.father.children(node.quad) = None  
     // Insert children first
-    for (q <- usedQuadrants) {
-      val child = toNonEmpty(node.child(q)) // Safe cast
-      reconsider(child, newRoot)
+    for (q <- usedQuadrants if node hasChild q) {
+      reconsider(node.child(q), newRoot) // Safe
     }
     // Insertion
-    val quad = newRoot.insertQuad(node.keys)
+    val quad = newRoot insertQuad node
     if (quad != worstQuad) insert(node, newRoot)
   }
   
   // Reinsert 
   // -----------
 
-  private def reinsert(node: QuadNode, newRoot: NonEmpty) {
-    if (!node.isEmpty) {
-      val nonEmpty = toNonEmpty(node) // Safe cast    
-      // Insert children first
-      for (q <- usedQuadrants) {
-        reinsert(node.child(q), newRoot)
-      }
-      // Insertion
-      insert(newRoot, nonEmpty)
+  private def reinsert(node: QuadNode, newRoot: QuadNode) {
+    //Detach
+    if (node.hasFather) node.father.children(node.quad) = None
+    // Insert children first
+    for (q <- usedQuadrants if node hasChild q) {
+      reinsert(node.child(q), newRoot)
     }
+    // Insertion
+    insert(newRoot, node)
   }
 
-  private def insert(node: NonEmpty, newRoot: NonEmpty) {
-    val quad = newRoot.insertQuad(node.keys)
+  private def insert(node: QuadNode, newRoot: QuadNode) {
+    val quad = newRoot insertQuad node
     // Keep searching
-    if (newRoot.hasChild(quad)) insert(node, toNonEmpty(newRoot.child(quad))) // Safe cast
+    if (newRoot hasChild quad) insert(node, newRoot.child(quad)) // Safe
     // Insert
     else {
-      newRoot.children(quad) = node
+      newRoot.children(quad) = Some(node)
       node.father = newRoot
       node.quad = quad
     } 
@@ -198,38 +173,35 @@ class HabenichtQuadTree[V](private val nDim: Int) {
   // Delete 
   // ------
   
-  private def delete(oldRoot: NonEmpty): QuadNode =  {
+  private def delete(oldRoot: QuadNode): Option[QuadNode] =  {
     val first = firstChild(usedQuadrants.min, oldRoot)
-    if (first == worstQuad) Empty // No children
+    if (first == Quadrants.max) None // No children
     else {
-      val newRoot = toNonEmpty(oldRoot.child(first)) // Safe cast
+      val newRoot = oldRoot.child(first) // Safe 
       move(newRoot, oldRoot)
-      for (q <- first+1 until worstQuad) {
+      for (q <- first+1 until Quadrants.max if oldRoot hasChild q) {
         reinsert(oldRoot.child(q), newRoot)
       }
-      newRoot
+      Some(newRoot)
     }
   }
 
-  private def firstChild(quad: Int, node: NonEmpty): Int = {
-    if (quad == worstQuad) worstQuad
+  private def firstChild(quad: Int, node: QuadNode): Int = {
+    if (quad == Quadrants.max) Quadrants.max
     else if (node.hasChild(quad)) quad
     else firstChild(quad + 1, node)
   }
     
-  def deleteNode(node: NonEmpty) {
-    if (node.father.isEmpty) root = Empty
-    else {
-      val father = toNonEmpty(node.father) // Safe cast
-      father.children(node.quad) = Empty
-    }
+  def deleteNode(node: QuadNode) {
+    if (!node.hasFather) root = None
+    else node.father.children(node.quad) = None // Safe
   }
   
-  def move(newNode: NonEmpty, oldNode: NonEmpty) {
-    if (oldNode.father.isEmpty) root = newNode
+  def move(newNode: QuadNode, oldNode: QuadNode) {
+    if (!oldNode.hasFather) root = Some(newNode)
     else {
-      val father = toNonEmpty(oldNode.father) // Safe cast
-      father.children(oldNode.quad) = newNode
+      val father = oldNode.father // Safe 
+      father.children(oldNode.quad) = Some(newNode)
       newNode.father = father
       newNode.quad = oldNode.quad
     }
@@ -239,26 +211,40 @@ class HabenichtQuadTree[V](private val nDim: Int) {
   // -----
 
   // Returns the opposite quadrant
-  private def opposite(quadId: Int): Int = quadId ^ worstQuad
+  private def opposite(quadId: Int): Int = quadId ^ Quadrants.max
 
   // Returns the list of quadrants that could contain best solutions
-  private def asGood(quad: Int): IndexedSeq[Int] = {
-    (quad until nQuadrants).filter(q => (q - (q ^ quad)) == quad)
+  def asGood(quad: Int): IndexedSeq[Int] = {
+    (quad to usedQuadrants.max).filter(q => (q - (q ^ quad)) == quad)
+  }
+  
+  def better(quad: Int): IndexedSeq[Int] = {
+    (quad+1 to usedQuadrants.max).filter(q => (q - (q ^ quad)) == quad)
   }
 
   // Returns the list of quadrants that could contain worst solutions
-  private def asBad(quad: Int): IndexedSeq[Int] = {
+  def asBad(quad: Int): IndexedSeq[Int] = {
     val dim = opposite(quad)
-    (0 to quad).filter(q => ((q ^ dim) - q) == dim)
+    (usedQuadrants.min to quad).filter(q => ((q ^ dim) - q) == dim)
   }
+  
+  def worst(quad: Int): IndexedSeq[Int] = {
+    val dim = opposite(quad)
+    (usedQuadrants.min until quad).filter(q => ((q ^ dim) - q) == dim)
+  }
+}
 
-  // Returns the lists of the sub-quadrants of quad that are:
-  // list 1 : non-adjacent to the worst quadrant
-  // list 2 : adjacent to the worst quadrant
-  private def adjacents(quad: Int): (IndexedSeq[Int], IndexedSeq[Int]) = {
-    val sameDims = opposite(quad)
-    Quadrants.partition(q => (q - (q ^ sameDims)) != sameDims)
-  }
-  // Shortcut for pattern matching
-  private def toNonEmpty(tree: QuadNode): NonEmpty = tree.asInstanceOf[NonEmpty]
+object HabenichtQuadTree extends App {
+  
+  val tree = new HabenichtQuadTree[String](3)
+  
+  tree.insert(Array(9, 0, 4), "A")
+  tree.insert(Array(6, 0, 7), "B")
+  tree.insert(Array(4, 1, 8), "C")
+  tree.insert(Array(4, 5, 7), "D")
+  tree.insert(Array(3, 5, 8), "E")
+  tree.insert(Array(5, 6, 6), "F")
+  tree.insert(Array(6, 3, 5), "G")
+  tree.insert(Array(6, 4, 8), "H")
+  println("Finish")
 }
