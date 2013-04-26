a8896edc20676525c30f56090cfac9ddf7d5084e
Fran?ois Pelsser <oscar@frol.be>
2013-04-26 15:55:03 +0200

    adding BinPackingFlowExtended

--- src/main/scala/oscar/visual/VisualToolBar.scala
+++ src/main/scala/oscar/visual/VisualToolBar.scala
@@ -7,7 +7,7 @@ import java.awt.event.ActionEvent
 
 class VisualToolBar extends JToolBar {
 
-  def addButton(label:String, actionOnClick : => Unit){
+  def addButton(label:String,actionOnClick : => Unit){
     val button = new JButton(label)
     button.addActionListener( new ActionListener() {
 		override def actionPerformed(e:ActionEvent) { actionOnClick }
@@ -15,4 +15,25 @@ class VisualToolBar extends JToolBar {
 	add(button)
 	button
   }
+}
+
+object VisualToolBar{
+  	
+  def main(args : Array[String]) {
+		val f = new VisualFrame("toto");
+		val tb = f.createToolBar
+		val d = new VisualDrawing(false);
+		val inf = f.createFrame("Drawing");
+		inf.add(d);
+		f.pack();
+		
+		tb.addButton("rectangle",{
+		  val rect = new VisualRectangle(d, 50, 50, 100, 50);
+		  rect.toolTip_$eq("Hello");
+		  
+		  tb.addButton("make it move",{
+			  rect.move(100, 20);
+		  })
+		})		
+	}
 }
