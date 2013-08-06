74bddcc7b6192ffe7952525d49deed900943ec0c
hg <devnull@localhost>
2013-08-06 12:42:30 +0200

    parser

---
+++ src/main/scala/oscar/cp/minizinc/Parser.scala
@@ -0,0 +1,152 @@
+package oscar.cp.minizinc
+
+import scala.util.parsing.combinator._
+import FZType._
+
+class Parser extends JavaTokenParsers {// RegexParsers {
+	var model : Minizinc_model = new Minizinc_model
+	
+	def flatzinc_model : Parser[Any] = rep(pred_decl)~rep(param_decl)~rep(var_decl)~rep(constraint)~solve_goal
+	def pred_decl : Parser[Any] = "predicate"~identifier~"("~rep1sep(pred_param, ",")~");"
+
+	def identifier : Parser[Any] = index_set
+	
+	def pred_param : Parser[Any] = pred_param_type~":"~pred_ann_id // what about no space before the ":" and one after ?
+	def pred_param_type : Parser[Any] = par_pred_param_type | var_pred_param_type
+	def par_type : Parser[Any] = (
+	    "bool"
+		| "float"
+		| "int"
+		| "set of int"
+		| "array"~opt(index_set)~"of bool"
+		| "array"~opt(index_set)~"of float"
+		| "array"~opt(index_set)~"of int"
+		| "array"~opt(index_set)~"of set of int"
+	)
+	def par_pred_param_type : Parser[Any] = (
+		par_type
+		| float_const~".."~float_const
+		| int_const~".."~int_const
+		| "{"~rep1sep(int_const, ",")~"}"
+		| "set of"~int_const~".."~int_const
+		| "set of"~"{"~rep1sep(int_const, ",")~"}"
+		| "array"~opt(index_set)~"of"~float_const~".."~float_const
+		| "array"~opt(index_set)~"of"~int_const~".."~int_const
+		| "array"~opt(index_set)~"of"~"{"~rep1sep(int_const, ",")~"}"
+		| "array"~opt(index_set)~"of"~"set of"~int_const~".."~int_const
+		| "array"~opt(index_set)~"of"~"set of"~"{"~rep1sep(int_const, ",")~"}"
+	)
+	def var_type : Parser[Any] = (
+	    "var"~int_const~".."~int_const
+		| "var float"
+	    | "var"~float_const~".."~float_const
+	    | "var int"
+	    | "var"~"{"~rep1sep(int_const, ",")~"}"
+	    | "var set of"~int_const~".."~int_const
+	    | "var set of"~"{"~rep1sep(int_const, ",")~"}"
+	    | "array"~opt(index_set)~"of var bool"
+	    | "array"~opt(index_set)~"of var float"
+	    | "array"~opt(index_set)~"of var"~float_const~".."~float_const
+	    | "array"~opt(index_set)~"of var int"
+	    | "array"~opt(index_set)~"of var"~int_const~".."~int_const
+	    | "array"~opt(index_set)~"of var"~"{"~rep1sep(int_const, ",")~"}"
+	    | "array"~opt(index_set)~"of var set of"~int_const~".."~int_const
+	    | "array"~opt(index_set)~"of var set of"~"{"~rep1sep(int_const, ",")~"}"
+	)
+	def var_pred_param_type : Parser[Any] = (
+		var_type
+		| "var set of int"
+		| "array"~opt(index_set)~"of var set of int"
+	)
+	def index_set : Parser[Any] = "1.."~int_const | "int" // what about the fact that "int" is only allowed in predicates ?
+	
+	def expr : Parser[Any] = (
+		bool_const
+		| set_const //should be float -> int -> set, inverted for set to work, need testing
+		| float_const
+		| int_const
+		| var_par_id
+		| var_par_id~opt(int_const) // why two entries for var_par_id ?
+		| array_expr
+		| annotation
+		| "...string constant..." //???
+	)
+	def pred_ann_id : Parser[Any] = "[A-Z_a-z][A-Z_a-z0-9_]*".r
+	
+	def var_par_id : Parser[String] = "-*[A-Za-z][A-Za-z0-9_]*".r
+	
+	def bool_const : Parser[Boolean] = (
+	    "true" ^^ (x => true)
+	    | "false" ^^ (x => false)
+	)
+	
+	def float_const : Parser[Float] = (
+	    int_const~"."~"[0-9][0-9]*".r~opt("[eE]".r~int_const) ^^ (_.toString.toFloat)
+	    | int_const~"[eE]".r~int_const ^^ (_.toString.toFloat)
+	)
+	
+	def int_const : Parser[Int] = "[0-9][0-9]*".r ^^ (_.toInt)// [+-] at the begining of the regex in the grammar, what does that mean ?
+	
+	def set_const : Parser[Any] = ( 
+	    int_const~".."~int_const ^^ { 
+	    	case int1~".."~int2 => Range(int1, int2, 1) 
+	    } 
+	    | "{"~>rep1sep(int_const, ",")<~"}" 
+	)
+	
+	def array_expr : Parser[Any] = "[]" | "["~rep1sep(expr, ",")~"]"
+	
+	def param_decl : Parser[Any] = par_type~":"~var_par_id~"="~expr~";" ^^ 
+	{
+	  case tp~":"~id~"="~e~";" =>
+	    tp match {
+	      case "bool" => model.dict += 
+	        ((id.toString, (FZType.P_BOOL, 
+	            new ParamBool(e.toString.toBoolean, id))))
+	      case "int" => model.dict += 
+	        ((id.toString, (FZType.P_INT, 
+	            new ParamInt(e.toString.toInt, id))))
+	      case "float" => model.dict += 
+	        ((id.toString, (FZType.P_FLOAT,
+	            new ParamFloat(e.toString.toFloat, id)))) // ! no floats in oscar, what to do ?
+	      case "set of int" => model.dict += 
+	        ((id.toString, (FZType.P_SET_INT,
+	          e match {
+	          	case x:Range => new ParamSetOfInt(e, true, id)
+	          	case x:List[Any] => new ParamSetOfInt(e, false, id)
+	          }
+	        )))
+	    }
+	}
+	  //the expr has restriction... what about it ?
+	
+	def var_decl : Parser[Any] = var_type~":"~var_par_id~annotations~opt("="~expr)~";" ^^
+	{
+	  
+//to be done when assignment of var is understood
+//	  case tp~":"~id~ann~Some("="~e)~";" => model.dict.get(id) match {
+//	    case Some((tp, fzo)) => model.dict += ((id, (tp, fzo)))
+//	  }
+	  case tp~":"~id~ann~None~";" => 
+	    tp match {
+	      case "var"~i1~".."~i2 => model.dict +=
+	        ((id, (FZType.V_INT_RANGE,
+	            new VarIntRange(Range(i1.toString.toInt, i2.toString.toInt, 1), ann.toString, id))))
+	      case "var int" => model.dict += 
+	      	((id, (FZType.V_INT, 
+	      		new VarInt(0, ann.toString, id))))
+	    }
+	    
+	    
+	}// the vars in assignment must be declared earlier
+	
+	def constraint : Parser[Any] = "constraint"~pred_ann_id~"("~rep1sep(expr, ",")~")"~annotations~";"
+	def solve_goal : Parser[Any] = (
+	    "solve"~annotations~"satisfy;"
+	    | "solve"~annotations~"minimize"~expr~";"
+	    | "solve"~annotations~"maximize"~expr~";"
+	) // expr must be a var name of var array element
+	def annotations : Parser[Any] = rep("::"~annotation)
+	def annotation : Parser[Any] = pred_ann_id | pred_ann_id~"("~rep1sep(expr, ",")~")" // some notes, see syntax
+	
+}
