d690af0ece6994ac02e50e6169e0f2058552c536
ledebroux <devnull@localhost>
2013-08-07 08:41:07 +0200

    first test

--- src/test/scala/oscar/cp/test/minizinc/TestParamDecl.scala
+++ src/test/scala/oscar/cp/test/minizinc/TestParamDecl.scala
@@ -5,6 +5,8 @@ import org.scalatest.matchers.ShouldMatchers
 
 import oscar.cp.minizinc.Parser
 
+
+// /!\ check if myParseAll in oscar.cp.minizinc.Parser tries to parse a flat_zinc model
 class TestParamDecl extends FunSuite with ShouldMatchers {
 	 test ("mytest"){
 		//val input = "bool: name = true;"
