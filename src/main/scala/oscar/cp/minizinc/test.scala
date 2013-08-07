385c3fb1e658047b32266693f8a9ccf2ae187ceb
ledebroux <devnull@localhost>
2013-08-07 15:16:02 +0200

    test

--- src/main/scala/oscar/cp/minizinc/test.scala
+++ src/main/scala/oscar/cp/minizinc/test.scala
@@ -6,7 +6,11 @@ object test extends Parser with App{
 	def test {
 		//val input = "bool: name = true;"
     	//val input = new FileReader(args(0))
-    	val input = "array [1..4] of var 1..8: queens :: output_array([1..4]); solve satisfy;"
+    	val input = "var 2..3: x :: output_var;"+
+    			"var 1..4: y :: output_var;" +
+    			"constraint int_le(x, y);" +
+    			"constraint set_ne(x, y);" +
+    			"solve satisfy;"
 		println(myParseAll(input))
 		//myParseAll(input)
 		println(model.dict.toString)
