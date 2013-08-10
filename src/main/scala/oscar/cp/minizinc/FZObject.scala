4fecd605806474ac5e9faea18a42359f256c6556
rhartert <devnull@localhost>
2013-08-10 16:55:53 +0200

    Fixes accidental merge

--- src/main/scala/oscar/cp/minizinc/FZObject.scala
+++
@@ -1,7 +0,0 @@
-package oscar.cp.minizinc
-
-abstract class FZObject ( 
-    val name : String){
-  
-	//def setValue (v: Any)
-}
--- src/main/scala/oscar/cp/minizinc/Minizinc_model.scala
+++
@@ -1,8 +0,0 @@
-package oscar.cp.minizinc
-
-import scala.collection.mutable.HashMap
-import FZType._
-
-class Minizinc_model {
-	var dict = new HashMap[String, (FZType, FZObject)]
-}
