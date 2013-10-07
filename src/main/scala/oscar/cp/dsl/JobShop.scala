5b6798321e056801fbccb7e71f43cde27777e1f1
malorne <devnull@localhost>
2013-10-07 11:24:01 +0200

    Added three data instances for Job Shop Scheduling and a tentative modeling reader.

---
+++ src/main/scala/oscar/cp/dsl/JobShop.scala
@@ -0,0 +1,65 @@
+package oscar.cp.dsl
+
+import oscar.cp.modeling._
+import oscar.cp.core._
+import oscar.search._
+import oscar.cp.scheduling._
+import scala.io.Source
+import scala.collection.mutable.ArrayBuffer
+
+/*
+case class JobShop(cp: CPScheduler, activities: Array[Activity], jobs: Array[Int], requirements: Array[Int], resources: Array[UnitResource]) {
+  
+  def customize(function: => Unit){
+    function
+  }
+  
+}*/
+
+class JobShop(filepath: String) {
+  
+  //def apply(filepath: String): JobShop = {
+    
+    // Parsing
+    // ----------------------------------------------------------------------
+    val file = Source.fromFile(filepath).getLines
+    def nextLineAsInt = file.next.trim.split(" +").map(_.toInt)
+    
+    val Array(nbJobs, nbMachines) = nextLineAsInt
+    val jobs					=		new ArrayBuffer[Int]
+    val requirements 	= 	new ArrayBuffer[Int]
+		val durations	 		= 	new ArrayBuffer[Int]
+  
+		for (job <- 0 until nbJobs){
+			for (Array(req, dur) <- nextLineAsInt.grouped(2)){
+      	jobs += job
+  			requirements += req
+  			durations += dur
+			}
+		}
+  
+	  // Modeling	
+    // ----------------------------------------------------------------------
+		
+	  val horizon = durations.sum
+	  val cp = new CPScheduler(horizon)
+	
+	  // Activities & Resources
+	  val activities = Array.tabulate(requirements.length)(i => Activity(cp, durations(i)))
+	  val resources  = Array.tabulate(nbMachines)(r => UnitResource(cp))
+	  
+	  // Constraints
+	  def defaultConstraints {
+	    // Resource allocation
+	  	for (i <- 0 until activities.length) 
+	  		activities(i) needs resources(requirements(i))
+		  
+		  // Precedence constraints
+		  for (i <- 0 until activities.length - 1; if (jobs(i) == jobs(i + 1)))
+		      activities(i) precedes activities(i + 1)
+	  }
+	  
+	  //new JobShop(cp, activities, jobs, requirements, resources)
+	//}
+	  
+}
---
+++ src/main/scala/oscar/cp/dsl/MyJobShop.scala
@@ -0,0 +1,38 @@
+package oscar.cp.dsl
+
+import oscar.visual._
+import oscar.cp.modeling._
+
+object MyJobShop extends JobShop("data/memScheduling/jobshop/ft10") with App {
+
+  	// Visualization  
+  	// -----------------------------------------------------------------------
+
+	  val frame = new VisualFrame("Job-Shop Problem", 2, 1)
+	  val colors = VisualUtil.getRandomColors(resources.length, true)
+	  
+	  val gantt1 = new VisualGanttChart(activities, i => jobs(i), colors = i => colors(requirements(i)))
+	  val gantt2 = new VisualGanttChart(activities, i => requirements(i), colors = i => colors(requirements(i)))
+	
+	  frame.createFrame("Gantt chart").add(gantt1)
+	  frame.createFrame("Resources utilization").add(gantt2)
+	  frame.pack
+	  
+	  
+	  // The makespan to minimize
+	  val makespan = maximum(activities)(_.end)
+	  cp.minimize(makespan) subjectTo defaultConstraints
+	  
+	  cp.exploration {
+	    for (r <- (0 until resources.length).sortBy(-resources(_).criticality).suspendable) {
+	      resources(r).rank()
+	    }
+	  
+	    cp.binary(Array(makespan))
+	    gantt1.update(1, 20)
+	    gantt2.update(1, 20)
+	  
+	  } run()
+	
+	  cp.printStats()
+}
