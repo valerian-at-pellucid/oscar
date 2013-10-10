90921dcb991b00449851f572526e6fb5bda26d71
Monjoie David <david.monjoie@student.uclouvain.be>
2013-10-10 21:31:30 +0200

    Added a part of the Semiconductor Testing data instances, along with its respectful reader.

---
+++ src/main/scala/oscar/cp/dsl/instances/SemiconductorTestingInstance.scala
@@ -0,0 +1,25 @@
+package oscar.cp.dsl.instances
+
+import oscar.cp.dsl.InstanceReader
+
+class SemiconductorTestingInstance(filepath: String) extends InstanceReader(filepath) {
+  
+  val Array(nbOperations, nbLots, nbMachines) = readLine asInt
+  
+  val operationsValues = 6		// Operations are described by 6 values.
+  
+  val Array(
+      	operations, 
+      	operationNumbers, 
+      	lotNumbers, 
+      	predecessors, 
+      	jobOperationTime, 
+      	jobDueDate,
+      	requiredMachines
+  		) = readDatas(nbOperations, operationsValues) asInt
+  		
+  val setupTimesValues = 3	// Setup times are described by 3 values.
+  
+  val Array(setupTimeArcs, i, j, setupTimes) = readDatas(allRemaining, setupTimesValues) asInt
+
+}
