48856f75070cfdf9b5ccbc828cf87338f1dfa044
Monjoie David <david.monjoie@student.uclouvain.be>
2013-10-10 12:28:27 +0200

    Corrected comments of method readDatas.

--- src/main/scala/oscar/cp/dsl/InstanceReader.scala
+++ src/main/scala/oscar/cp/dsl/InstanceReader.scala
@@ -28,7 +28,8 @@ class InstanceReader(filepath: String) {
   /**
    * Reads lines of the file by batches and builds an array of nbColumns+1 arrays containing
    * the nbColumns values describing each of the nbElement elements, prepended of the array
-   * of elements indices.
+   * of elements indices. If the end of file is reached, the resulting arrays may not contain
+   * nbElements elements. This is obviously the case when using the value allRemaining.
    * For each element, it will read lines until it has read nbColumns values.
    * The first array contains the batch numbers from which the other datas have been read.
    * The following arrays contains the datas read from the nbColumns columns.
@@ -44,8 +45,8 @@ class InstanceReader(filepath: String) {
    * 0 9 1
    * 4 2
    * 
-   * Will try to read 5 columns in batches of 2 lines and will lead to the following array:
-   * [[0 1]		// Batches index other datas are coming from. In this example we read 3 bathes of 2 lines.
+   * Will try to read 2 elements described by 5 columns and will lead to the following array:
+   * [[0 1]		// Batches index other datas are coming from. In this example we read 2 batches of 2 lines.
    *  [2 8]		// Values found in the first column of the three batches of lines we read. 
    *  [4 0]		// Values found in the second column of the three batches of lines we read.
    *  [6 2]		// Values found in the third column of the three batches of lines we read.
@@ -60,7 +61,7 @@ class InstanceReader(filepath: String) {
       val readValues = new ListBuffer[String]
 	    while (readValues.length < nbColumns && file.hasNext) {
 	      val line = nextLine
-	      // TODO Test if line is comment or something else!
+	      // TODO Test if line is comment or something else, if it is actually valid!
 	      if (! "".equals(line)) readValues ++= line.split(splitRegexp)
 	    }
 	    
