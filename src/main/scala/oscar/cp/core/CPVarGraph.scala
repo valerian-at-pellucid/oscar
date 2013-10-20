67f5f23a074c3ef4a4e71bd18488a89489e08f78
Andrew Lambert <andrew.lambert@student.uclouvain.be>
2013-10-20 18:44:15 +0200

    implement some accessors

--- src/main/scala/oscar/cp/core/CPVarGraph.scala
+++ src/main/scala/oscar/cp/core/CPVarGraph.scala
@@ -14,6 +14,7 @@
  ******************************************************************************/
 package oscar.cp.core
 
+
 /**
  * @author Andrew Lambert andrew.lambert@student.uclouvain.be
  */
@@ -56,31 +57,84 @@ class CPVarGraph(val s: CPStore, nNodes: Int, inputEdges: List[(Int,Int)], val n
   }
   
    /**
-   * Gives mandatory outer edges from a node n
-   * @param node
-   * @return Return an array with the index of all mandatory outgoing edges
+   * @param node Id
+   * @return Return an array with the index of all mandatory outgoing edges from the node
    */
-  def mandatoryOutEdges(node : Int) : Array[Int] = Array()
+  def mandatoryOutEdges(nodeId: Int) : Array[Int] = nodes(nodeId).outEdges.filter( E isRequired _ ).toArray
   
   /**
-   * Gives mandatory inner edges from a node n
-   * @param node
-   * @return Return an array with the index of all mandatory incoming edges
+   * @param node Id
+   * @return Return an array with the index of all mandatory incoming edges from the node
    */
-  def mandatoryInEdges(node : Int) : Array[Int] = Array()
-  
-  /**
-   * Gives possible outer edges from a node n
-   * @param node
-   * @return Return an array with the index of all possible outgoing edges
-   */
-  def possibleOutEdges(node : Int) : Array[Int] = Array()
-  
-  /**
-   * Gives possible inner edges from a node n
-   * @param node
-   * @return Return an array with the index of all possible incoming edges
-   */
-  def possibleInEdges(node : Int) : Array[Int] = Array()
+  def mandatoryInEdges(nodeId: Int) : Array[Int] = nodes(nodeId).inEdges.filter( E isRequired _ ).toArray
 
+  /**
+   * @param node Id
+   * @return Return an array with the index of all mandatory edges from the node
+   */
+  def mandatoryEdges(nodeId: Int) : Array[Int] = mandatoryInEdges(nodeId) ++ mandatoryOutEdges(nodeId)
+  
+  /**
+   * @param node Id
+   * @return Return an array with the index of all possible outgoing edges from the node
+   */
+  def possibleOutEdges(nodeId: Int) : Array[Int] = nodes(nodeId).outEdges.filter( E isPossible _ ).toArray
+  
+  /**
+   * @param node Id
+   * @return Return an array with the index of all possible incoming edgesfrom the node
+   */
+  def possibleInEdges(nodeId: Int) : Array[Int] = nodes(nodeId).inEdges.filter( E isPossible _ ).toArray
+
+  /**
+   * @param node Id
+   * @return Return an array with the index of all possible edges from the node
+   */
+  def possibleEdges(nodeId: Int) : Array[Int] = possibleInEdges(nodeId) ++ possibleOutEdges(nodeId)
+  
+  /* TODO : HANDLE ERROR CASES IN ADD/REMOVE FUNCTIONS */
+  
+  /**
+   * Add a node into mandatory nodes from graph interval
+   * @param node Id
+   */
+  def addMandatoryNode(nodeId: Int) : CPOutcome = N.requires(nodeId)
+  
+  /**
+   * Remove a node from possible nodes from graph interval
+   * @param node Id
+   */
+  def removeNode(nodeId: Int) : CPOutcome = N.excludes(nodeId)
+  
+  /**
+   * Add an edge into mandatory edges from graph interval
+   * @param source and destination of the edge
+   */
+  def addMandatoryEdge(src: Int, dest: Int) : CPOutcome = {
+    val index = indexOfEdge(src, dest)
+    if (index == -1) CPOutcome.Failure
+    else E.requires(index)
+  }
+  
+  /**
+   * Remove an edge from possible edges from graph interval
+   * @param source and destination of the edge
+   */
+  def removeEdge(src: Int, dest: Int) : CPOutcome = {
+    val index = indexOfEdge(src, dest)
+    if (index == -1) CPOutcome.Failure
+    else E.excludes(index)
+  }
+  
+  /**
+   * @return the index of the edge (src,dest)
+   * 			-1 if the edge is not in the graph interval
+   */
+  private def indexOfEdge(src: Int, dest: Int) : Int = {
+    val edge = edges.filter(e => (e.src == src && e.dest == dest))
+    if (edge.isEmpty) -1
+    else edge(0).i
+  }
+  
 }
+
