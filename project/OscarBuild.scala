ea93cbe2576c2142fcc7f13c40e9cba25d65155b
pschaus <devnull@localhost>
2013-11-21 21:52:53 +0100

    fix scala-test problem in build

--- project/OscarBuild.scala
+++ project/OscarBuild.scala
@@ -39,7 +39,7 @@ object OscarBuild extends Build {
 
   object Dependencies {
 
-    val scalatest = "org.scalatest" % "scalatest_2.10" % "2.0" % "test"
+    val scalatest =  "org.scalatest" %% "scalatest" % "2.0.M5b" 
     val junit = "junit" % "junit" % "4.8.1" % "test"
     val scalaswing = "org.scala-lang" % "scala-swing" % "2.10.0"
 
