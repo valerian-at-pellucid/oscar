869d74a1e7484a18387ef9159add71a5284a444a
pschaus <devnull@localhost>
2013-11-25 10:48:55 +0100

    modify sbt build to specify one unique unmanaged jar directory http://stackoverflow.com/questions/20139366/how-to-share-an-unmanaged-directory-between-several-sub-projects

--- project/OscarBuild.scala
+++ project/OscarBuild.scala
@@ -40,9 +40,27 @@ object OscarBuild extends Build {
 
   object Dependencies {
 
-    val scalatest =  "org.scalatest" %% "scalatest" % "2.0.M5b" 
+    val scalatest = "org.scalatest" %% "scalatest" % "2.0.M5b"
     val junit = "junit" % "junit" % "4.8.1" % "test"
     val scalaswing = "org.scala-lang" % "scala-swing" % "2.10.0"
+
+    // DSL for adding source dependencies ot projects.
+    def dependsOnSource(dir: String): Seq[Setting[_]] = {
+      import Keys._
+      Seq(unmanagedSourceDirectories in Compile <<= (unmanagedSourceDirectories in Compile, baseDirectory in ThisBuild) { (srcDirs, base) => (base / dir ) +: srcDirs },
+          unmanagedJars in Compile <++= (baseDirectory in ThisBuild) map { base =>
+          	val libs = base / dir
+          	val dirs = libs // (libs / "batik") +++ (libs / "libtw") +++ (libs / "kiama")
+          	(dirs ** "*.jar").classpath
+      	   },
+          
+          unmanagedSourceDirectories in Test <<= (unmanagedSourceDirectories in Test, baseDirectory in ThisBuild) { (srcDirs, base) => (base / dir ) +: srcDirs })
+    }
+    implicit def p2source(p: Project): SourceDepHelper = new SourceDepHelper(p)
+    final class SourceDepHelper(p: Project) {
+      def dependsOnSource(dir: String): Project =
+        p.settings(Dependencies.dependsOnSource(dir): _*)
+    }
 
   }
   
@@ -81,95 +99,82 @@ object OscarBuild extends Build {
   //jacoco.reportFormats in jacoco.Config := Seq(XMLReport("utf-8"), HTMLReport("utf-8"))
   
   
-
-  
   lazy val oscar = Project(
     id = "oscar",
     base = file("."),
     //
     settings = buildSettings ++ jacoco_settings ++ packSettings ++ unidocSettings ++ Seq (/*resolvers := sbtResolvers,*/ libraryDependencies ++= commonDeps) ++ commonTasks,
-    aggregate = Seq(oscarVisual,oscarCp,oscarCbls,oscarLinprog,oscarDes,oscarDfo))
+    aggregate = Seq(oscarVisual,oscarCp,oscarCbls,oscarLinprog,oscarDes,oscarDfo)) dependsOnSource("lib")
 
 
   lazy val oscarExamples = Project(
     id = "oscar-examples",
     base = file("oscar-examples"),
     settings = buildSettings ++ jacoco_settings ++ Seq(libraryDependencies ++= commonDeps) ++ commonTasks,
-    dependencies = Seq(oscarVisual,oscarCp,oscarCbls,oscarLinprog,oscarDes,oscarDfo))         
+    dependencies = Seq(oscarVisual,oscarCp,oscarCbls,oscarLinprog,oscarDes,oscarDfo)) dependsOnSource("lib")         
     
     
   lazy val oscarCbls = Project(
     id = "oscar-cbls",
     base = file("oscar-cbls"),
     settings = buildSettings ++ jacoco_settings ++ Seq(libraryDependencies ++= commonDeps) ++ commonTasks,
-    dependencies = Seq(oscarVisual))       
+    dependencies = Seq(oscarVisual)) dependsOnSource("lib")       
     
   lazy val oscarCp = Project(
     id = "oscar-cp",
     base = file("oscar-cp"),
     settings = buildSettings ++ jacoco_settings ++ Seq(libraryDependencies ++= commonDeps) ++ commonTasks,
-    dependencies = Seq(oscarAlgo,oscarVisual))  
+    dependencies = Seq(oscarAlgo,oscarVisual)) dependsOnSource("lib") 
     
   lazy val oscarDes = Project(
     id = "oscar-des",
     base = file("oscar-des"),
     settings = buildSettings ++ jacoco_settings ++ Seq(libraryDependencies ++= commonDeps) ++ commonTasks,
-    dependencies = Seq(oscarInvariants))       
+    dependencies = Seq(oscarInvariants)) dependsOnSource("lib")     
     
   lazy val oscarDfo = Project(
     id = "oscar-dfo",
     base = file("oscar-dfo"),
     settings = buildSettings ++ jacoco_settings ++ Seq(libraryDependencies ++= commonDeps) ++ commonTasks,
-    dependencies = Seq(oscarAlgebra,oscarVisual))        
+    dependencies = Seq(oscarAlgebra,oscarVisual)) dependsOnSource("lib")       
     
-  lazy val oscarLinprog = Project(
+  lazy val oscarLinprog = Project( 
     id = "oscar-linprog",
     base = file("oscar-linprog"),
     settings = buildSettings ++ jacoco_settings ++ Seq(libraryDependencies ++= commonDeps) ++ commonTasks,
-    dependencies = Seq(oscarAlgebra))   
+    dependencies = Seq(oscarAlgebra)
+    ) dependsOnSource("lib")
+    
 
-  oscarLinprog.settings(unmanagedBase <<= baseDirectory { base => base.asFile.getParentFile() } )    
-    
-   /* 
-  oscarLinprog.settings(unmanagedBase <<= baseDirectory { 
-    println("yahoo")
-    base => {
-      println(base.asFile.getParent())
-      base.asFile.getParentFile() } 
-    })
-  */
-    
-  oscarLinprog.settings(unmanagedBase := file("libo"))
-  
   lazy val oscarAlgo = Project(
     id = "oscar-algo",
     settings = buildSettings ++ jacoco_settings ++ Seq (libraryDependencies ++= commonDeps) ++ commonTasks,    
     base = file("oscar-algo"),
-    dependencies= Seq(oscarUtil,oscarVisual))
+    dependencies= Seq(oscarUtil,oscarVisual)) dependsOnSource("lib")
     
   lazy val oscarVisual = Project(
     id = "oscar-visual",
     settings = buildSettings ++ jacoco_settings ++ Seq (libraryDependencies ++= commonDeps) ++ commonTasks,    
     base = file("oscar-visual"),
-    dependencies= Seq(oscarUtil))        
+    dependencies= Seq(oscarUtil)) dependsOnSource("lib")      
 
   lazy val oscarInvariants = Project(
     id = "oscar-invariants",
     settings = buildSettings ++ jacoco_settings ++ Seq (libraryDependencies ++= commonDeps) ++ commonTasks,    
-    base = file("oscar-invariants"))      
+    base = file("oscar-invariants")) dependsOnSource("lib")     
  
  
   lazy val oscarAlgebra = Project(
     id = "oscar-algebra",
     settings = buildSettings ++ jacoco_settings ++ Seq (libraryDependencies ++= commonDeps) ++ commonTasks,    
-    base = file("oscar-algebra"))       
+    base = file("oscar-algebra")) dependsOnSource("lib")     
 
       
     
   lazy val oscarUtil = Project(
     id = "oscar-util",
     settings = buildSettings ++ jacoco_settings ++ Seq (libraryDependencies ++= commonDeps) ++ commonTasks,    
-    base = file("oscar-util"))        
+    base = file("oscar-util")) dependsOnSource("lib")       
     
 
     
