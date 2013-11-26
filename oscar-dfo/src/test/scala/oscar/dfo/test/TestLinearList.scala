642ccfadcd4710be411f4f033b200607ff765a4a
pschaus <devnull@localhost>
2013-11-26 14:14:47 +0100

    move test located at wrong place

--- oscar-dfo/src/test/scala/oscar/dfo/test/TestLinearList.scala
+++ oscar-dfo/src/test/scala/oscar/dfo/test/TestLinearList.scala
@@ -12,7 +12,7 @@
  * You should have received a copy of the GNU Lesser General Public License along with OscaR.
  * If not, see http://www.gnu.org/licenses/lgpl-3.0.en.html
  ******************************************************************************/
-package oscar.util.mo.test
+package oscar.dfo.test
 
 import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
