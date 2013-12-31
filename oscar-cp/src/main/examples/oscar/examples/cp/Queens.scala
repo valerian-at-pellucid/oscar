8847e8ed5bf5feb0f070f3f6104731c84e4ae93d
rhartert <devnull@localhost>
2013-12-31 21:14:24 +0100

    CPModel does not extend App anymore

--- oscar-cp/src/main/examples/oscar/examples/cp/Queens.scala
+++ oscar-cp/src/main/examples/oscar/examples/cp/Queens.scala
@@ -8,10 +8,11 @@ import oscar.util._
  * n-queens model: place n-queens on a chess-board such that they don't attack each other.
  * this program search for all the solutions
  * Using Non Deterministic Search
+ * 
  * @author Pierre Schaus pschaus@gmail.com
  * @author Renaud Hartert ren.hartert@gmail.com
  */
-object Queens extends CPModel {
+object Queens extends CPModel with App {
 
   val nQueens = 12 // Number of queens
   val Queens = 0 until nQueens
