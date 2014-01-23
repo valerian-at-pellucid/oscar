6c1841df9d9d5495777296ba69b351a29efdf7cc
pschaus <devnull@localhost>
2014-01-23 10:07:17 +0100

    minor changes in objective, allow to clear the onSolution callbacks, remove failure when objective are not bound and objective filtering is disable

--- oscar-algo/src/main/scala/oscar/algo/search/Search.scala
+++ oscar-algo/src/main/scala/oscar/algo/search/Search.scala
@@ -25,7 +25,7 @@ class SearchStatistics(
     val timeInTrail: Long,
     val maxTrailSize: Int,
     val nSols: Int) {
-  override val toString: String = s"nNodes: $nNodes\nnFails: $nFails\ntime(ms): $time\ncompleted: $completed\ntimeInTrail: $timeInTrail\n$nSols: $nSols\n"
+  override val toString: String = s"nNodes: $nNodes\nnFails: $nFails\ntime(ms): $time\ncompleted: $completed\ntimeInTrail: $timeInTrail\nnSols: $nSols\n"
 }
 
 
