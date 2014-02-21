f870f24826cd41e71c9de84db9fad76d0e198fe0
rdl <rdl@lt-rdl2.cetic.be>
2014-02-21 13:22:01 +0100

    the new propagationStructure that will not get sporadic cycle in SCC. To test!

--- oscar-cbls/src/main/scala/oscar/cbls/invariants/core/propagation/PropagationStructure.scala
+++ oscar-cbls/src/main/scala/oscar/cbls/invariants/core/propagation/PropagationStructure.scala
@@ -95,14 +95,6 @@ abstract class PropagationStructure(val verbose: Boolean, val checker:Option[Che
     */
   def isAcyclic: Boolean = acyclic
 
-  /**return the summed stalls of all SCC.
-    * A stall is when the SCC is unable to maintain the topological sort incrementally,
-    * and must recompute it from scratch
-    * this happens when dependencies are modified with transient cycles
-    * @return the summed number of stalls for all the SCC
-    */
-  def getStalls = StronglyConnexComponentsList.foldLeft(0)((acc,scc) => acc + scc.getStalls)
-
   private var StronglyConnexComponentsList: List[StronglyConnectedComponent] = List.empty
 
   /**To call when one has defined all the propagation elements on which propagation will ever be triggered.
@@ -569,24 +561,58 @@ class StronglyConnectedComponent(val Elements: Iterable[PropagationElement],
     super.scheduleForPropagation()
   }
 
-  var Stalls = 0
-  def getStalls = Stalls
+  def filterForDelayedInjectionListened(listening:PropagationElement):(PropagationElement,=>Unit, => Boolean) => Unit = {
+    def filter(listened:PropagationElement,injector:(=>Unit), isStillValid:(=> Boolean)){
+      if(listening.component == listened.component)
+        this.registerListenedWaitingDependency(injector, isStillValid)
+    }
+    filter
+  }
 
-  def addDependency(from:PropagationElement, to:PropagationElement){
-    if(autoSort){
-      try{
-        notifyAddEdge(from,to)
-      }catch{
-        case c:CycleException => {
-          //This can happen if we perform heavy changes to the dependencies in a careless way,
-          // eg: reloading a previous model.
-          // We wait for the dependencies to be stable, when the propagation is performed.
-          println("cycle in SCC, reverting to differed non-incremental sort")
-          autoSort = false
-          Stalls +=1
-        }
+  def filterForDelayedInjectionListening(listened:PropagationElement):((PropagationElement,Any),=>Unit, => Boolean) => Unit = {
+    def filter(listeningAndPayload:(PropagationElement,Any),injector:(=>Unit), isStillValid:(=> Boolean)){
+      if(listeningAndPayload._1.component == listened.component)
+        this.registerListeningWaitingDependency(injector)
+    }
+    filter
+  }
+
+  var newDependenciesToInject:List[WaitingDependency] = List.empty
+
+  case class WaitingDependency(val from:PropagationElement,
+                               val to:PropagationElement,
+                               var inject1:(=>Unit) = null,
+                               var inject2:(=>Unit) = null,
+                               var isStillValid:(=>Boolean) = null)
+
+  def injectNewDependencies(autoSort:Boolean){
+    for(d:WaitingDependency <- newDependenciesToInject){
+      if(d.isStillValid){
+          d.inject1
+          d.inject2
+          if(autoSort) notifyAddEdge(d.from,d.to)
       }
     }
+    newDependenciesToInject = List.empty
+  }
+
+  def registerListenedWaitingDependency(injector:(=>Unit), isStillValid:(=>Boolean)){
+    val waiting = newDependenciesToInject.head
+    waiting.inject1 = injector
+    waiting.isStillValid = isStillValid
+  }
+
+  def registerListeningWaitingDependency(injector:(=>Unit)){
+    val waiting = newDependenciesToInject.head
+    waiting.inject2 = injector
+  }
+
+  def newWaitingDependency(from:PropagationElement, to:PropagationElement){
+    newDependenciesToInject = WaitingDependency(from,to) :: newDependenciesToInject
+  }
+
+  def addDependency(from:PropagationElement, to:PropagationElement){
+    newWaitingDependency(from:PropagationElement, to:PropagationElement)
   }
 
   val h: BinomialHeap[PropagationElement] = new BinomialHeap[PropagationElement](p => p.Position, size)
@@ -594,6 +620,8 @@ class StronglyConnectedComponent(val Elements: Iterable[PropagationElement],
   override def performPropagation() {
     //setting autosort to true will not perform any operation unless it was set to false. This happens in two cases:
     //at the initial propagation, and when a stall just occurred. In these case, a non-incremental sort takes place
+
+    injectNewDependencies(autoSort)
     autoSort = true
 
     for (e <- ScheduledElements) {
@@ -725,10 +753,21 @@ trait PropagationElement extends DAGNode with TarjanNode{
 
   def InitiateDynamicGraphFromSameComponent() {
     assert(component != null)
+
+    def filterForListened(listened:PropagationElement,injector:(=>Unit), isStillValid:(=> Boolean)){
+      if(component == listened.component)
+        component.registerListenedWaitingDependency(injector, isStillValid)
+    }
     DynamicallyListenedElementsFromSameComponent
-      = DynamicallyListenedElements.permaFilter((e: PropagationElement) => e.component == component)
+      = DynamicallyListenedElements.delayedPermaFilter(filterForListened)
+
+    def filterForListening(listeningAndPayload:(PropagationElement,Any),injector:(=>Unit), isStillValid:(=> Boolean)){
+      if(component == listeningAndPayload._1.component)
+        component.registerListeningWaitingDependency(injector)
+    }
+
     DynamicallyListeningElementsFromSameComponent
-      = DynamicallyListeningElements.permaFilter((e) => e._1.component == component, (e) => e._1)
+      = DynamicallyListeningElements.delayedPermaFilter(filterForListening, (e) => e._1)
   }
 
   /**through this method, the PropagationElement must declare which PropagationElement it is listening to
@@ -785,15 +824,17 @@ trait PropagationElement extends DAGNode with TarjanNode{
   protected def registerDynamicallyListenedElement(p: PropagationElement, i: Any): KeyForElementRemoval = {
     assert(isStaticPropagationGraphOrBulked(p, 1),
       "dependency to element " + p + " must be registered in static propagation graph before dynamic one")
-    val KeyForListeningElement = DynamicallyListenedElements.addElem(p)
-    val KeyForListenedElement = p.DynamicallyListeningElements.addElem((this, i))
 
     //We need to check for boundary here Because other elements might indeed change their dependencies,
     // but since they are not boundary, this would require resorting the SCC during the propagation
     if (IsBoundary && p.component == this.component) {
       //this is only called once the component is established, so no worries.
+      //we must call this before performing hte injection to create the waitingDependency in the SCC
       component.addDependency(p, this)
     }
+
+    val KeyForListeningElement = DynamicallyListenedElements.addElem(p)
+    val KeyForListenedElement = p.DynamicallyListeningElements.addElem((this, i))
 
     KeyForElementRemoval(p, KeyForListenedElement, KeyForListeningElement)
   }
