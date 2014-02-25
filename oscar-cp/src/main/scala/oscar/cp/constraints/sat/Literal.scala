c34f693e066527837ecaecb20ce13d3a5e74e210
rhartert <devnull@localhost>
2014-02-25 14:47:58 +0100

    adds Tarjan

--- oscar-cp/src/main/scala/oscar/cp/constraints/sat/Literal.scala
+++ oscar-cp/src/main/scala/oscar/cp/constraints/sat/Literal.scala
@@ -6,9 +6,13 @@ import oscar.cp.core.CPStore
 import oscar.cp.modeling.CPSolver
 import oscar.cp.core.Constraint
 
-abstract class Literal {
-  protected val boolean: CPBoolVar
-  def isTrue: Boolean = boolean.isTrue
+import oscar.cp.modeling.CPModel
+import oscar.cp.search.BinaryStaticOrderBranching
+
+abstract class Literal(val boolean: CPBoolVar) {
+  val id: Long
+  def isTrue: Boolean // = boolean.isTrue
+  def isFalse: Boolean // = boolean.isFalse
   def isBound: Boolean = boolean.isBound
   def set(b: Boolean): CPOutcome = if (b) setTrue else setFalse
   def setTrue: CPOutcome
@@ -18,34 +22,72 @@ abstract class Literal {
   def callValBindWhenAssigned(c: Constraint, id: Int): Unit = {
     boolean.callValBindIdxWhenBind(c, id)
   }
+  override def toString = boolean.toString
 }
 
-object BoolLiteral {
 
-  def apply(s: CPStore): Literal = new Literal {
-    override protected val boolean: CPBoolVar = CPBoolVar()(store)
-    override def setTrue: CPOutcome = boolean.assign(1)
-    override def setFalse: CPOutcome = boolean.assign(0)
-    override val negation: Literal = {
-      val lit = this
-      new Literal {
-        override protected val boolean: CPBoolVar = lit.boolean
-        override def setTrue: CPOutcome = lit.setFalse
-        override def setFalse: CPOutcome = lit.setTrue
-        override def isTrue: Boolean = !lit.isTrue
-        override val negation: Literal = lit
-        override val store = s
+object BoolLiteral {
+  private var literalCounter: Long = 0
+  
+  private def nextId(): Long = {
+    val id = literalCounter
+    literalCounter += 1
+    id
+  }
+  
+  def apply()(implicit s: CPStore): Literal = {
+    val b = CPBoolVar()
+    
+    new Literal(b) {
+      override val id = nextId()
+      override def setTrue: CPOutcome = b.assign(1)
+      override def setFalse: CPOutcome = b.assign(0)
+      override def isTrue: Boolean = b.isTrue
+      override def isFalse: Boolean = b.isFalse
+      override val negation: Literal = {
+        val lit = this
+        new Literal(b) {
+          override val id = nextId()
+          override def setTrue: CPOutcome = lit.setFalse
+          override def setFalse: CPOutcome = lit.setTrue
+          override def isTrue: Boolean = lit.isFalse
+          override def isFalse: Boolean = lit.isTrue
+          override val negation: Literal = lit
+          override val store = s
+        }
       }
+      override val store = s
     }
-    override val store = s
   }
 }
 
-object TestFoireux extends App {
 
-  val cp = CPSolver()
-  val b = CPBoolVar()(cp)
-  val lit = BoolLiteral(cp)
-  println("finish")
+import oscar.cp.modeling._
+object TestLiteral extends CPModel with App {
 
+  // val cp = CPSolver()
+  // val b = CPBoolVar() //(cp)
+  // val lit = BoolLiteral() //(cp)
+  
+  val lits = Array.tabulate(4)(i => BoolLiteral())
+//  println("lits was defined, is " + lits.mkString("[", "; ", "]"))
+  
+  search {
+    new BinaryStaticOrderBranching(lits.map(_.boolean))
+  }
+  
+  // val clauses = Array( (lits(0).negation, lits(0).negation), (lits(0), lits(0)) )  // a \/ a, -a \/ -a, this means a and -a simultaneously, infeasible
+  // val clauses = Array( (lits(0).negation, lits(1)), (lits(1).negation, lits(2)), (lits(2).negation, lits(0)) )  // a => b, b => c, c => a
+  // val clauses = Array(  (lits(0).negation, lits(1)), (lits(1).negation, lits(2)), (lits(2).negation, lits(0).negation) ) // a => b, b => c, c => -a
+  val clauses = Array(  (lits(0).negation, lits(1)), (lits(1).negation, lits(2)), (lits(2).negation, lits(3)), (lits(3).negation, lits(0).negation) ) // a => b, b => c, c => d, d => -a  
+  
+  add(new SatConstraint(lits ++ lits.map(_.negation), clauses))
+  
+  onSolution {
+    println("solution, lits = " + lits.mkString("[", "; ", "]"))
+  }
+  
+  val stats = start() 
+    
+  // println("finished, literal = " + lits.mkString("[", "; ", "]"))
 }
--- oscar-cp/src/main/scala/oscar/cp/constraints/sat/SatConstraint.scala
+++ oscar-cp/src/main/scala/oscar/cp/constraints/sat/SatConstraint.scala
@@ -6,23 +6,44 @@ import oscar.cp.core.CPOutcome
 import oscar.cp.core.CPOutcome._
 import oscar.cp.core.CPIntVar
 import scala.collection.mutable.Queue
+import scala.collection.immutable.Map
 
-class SatConstraint(val literals: Array[Literal]) extends Constraint(literals.head.store, "SatConstraint") {
+class SatConstraint(val literals: Array[Literal], val clauses: Array[(Literal, Literal)]) extends Constraint(literals.head.store, "SatConstraint") {
   
   private val nLiterals = literals.size
   private val Literals = 0 until nLiterals
   
+  def IDToIndex = literals.map(_.id).zipWithIndex.toMap
+//  println("IDToIndex = " + IDToIndex)
+  
   // Graph
-  private val litToSCC: Array[Int] = ???
-  private val SCCToLit: Array[Array[Int]] = ???
-  private val SCCToSCC: Array[Array[Int]] = ???
+  private val nClauses = clauses.size
+  private val implicationArcs = Array.tabulate(2 * nClauses) { i =>
+    if (i < nClauses) (clauses(i           )._1.negation, clauses(i           )._2)
+    else              (clauses(i - nClauses)._2.negation, clauses(i - nClauses)._1)
+  }
+    .map { case (l1, l2) => (IDToIndex(l1.id), IDToIndex(l2.id)) }
+    
+  private val implicationAdjacency = Array.tabulate(nLiterals) { i => Array.empty[Int] }
+  implicationArcs.groupBy(_._1).foreach { case (i, ijPairs) => implicationAdjacency(i) = ijPairs.map(_._2) }
+//  println("Implication Arcs = " + implicationArcs.mkString("(", ", ", ")"))
+    
+  private val implicationSccs = new Tarjan(Literals.toArray, implicationAdjacency)
+    
+  private val litToSCC = implicationSccs.sccOf
+  private val SCCToLit = implicationSccs.nodesOf
+  private val SCCToSCC = implicationSccs.successorsOf
+  
+//  println("litToSCC: " + litToSCC.mkString(", "))
+//  println("SCCToLit: " + SCCToLit.map(_.mkString(", ")).mkString("; "))
+//  println("SCCToSCC: " + SCCToSCC.map(_.mkString(", ")).mkString("; "))
   
   // Used for internal propagation
   private val queue: Queue[Int] = Queue()
   private val visited: Array[Boolean] = Array.fill(nLiterals)(false)
   
   override def setup(l: CPPropagStrength): CPOutcome = {
-    if (propagate() == Failure) Failure 
+    if (isInfeasible() || propagate() == Failure) Failure 
     else {
       Literals.foreach(l => literals(l).callValBindWhenAssigned(this, l))
       Suspend
@@ -31,6 +52,11 @@ class SatConstraint(val literals: Array[Literal]) extends Constraint(literals.he
   
   @inline
   private def propagation(lit: Int): CPOutcome = {
+ //   println("2SAT woken on literal of id " + literals(lit).id + ", which is " + literals(lit).boolean)
+    if (literals(lit).isFalse) {
+ //     println("Nothing to do")
+      return Suspend
+    }
     
     // Initializes the structure
     queue.clear()
@@ -40,33 +66,71 @@ class SatConstraint(val literals: Array[Literal]) extends Constraint(literals.he
     queue.enqueue(init)
     visited(init) = true
     
+    
     while (!queue.isEmpty) {
       val scc = queue.dequeue()
+ //     println("Visiting SCC " + scc)
       if (setSCC(scc, true) == Failure) return Failure
       else {
         val sccs = SCCToSCC(scc)
         for (s <- SCCToSCC(scc)) {
           if (!visited(s)) {
+ //           println("Adding SCC " + s + " to queue.")
             queue.enqueue(s)
             visited(s) = true
           }
         }
       }
     }
+ //   println("Suspending")
 
     Suspend
   }
   
+  private def isInfeasible(): Boolean = {
+    literals.exists { l => litToSCC(IDToIndex(l.id)) == litToSCC(IDToIndex(l.negation.id)) }
+  }
+  
   @inline
   private def setSCC(scc: Int, value: Boolean): CPOutcome = {
-    if (SCCToLit(scc).exists(literals(_).set(value) == Failure)) Failure
+    if (SCCToLit(scc).exists { i => 
+  //    println("Setting literals(" + i + ") to " + value) ; 
+      literals(i).set(value) == Failure 
+      }) {
+//      println("scc propagation failed")
+      return Failure
+    }
+      
     else Suspend
   }
   
-  override def valBindIdx(boolvar: CPIntVar, id: Int): CPOutcome = {
+  override def valBindIdx(boolvar: CPIntVar, index: Int): CPOutcome = {
     deactivate() // Do not register this constraint in L1
-    val outcome = propagation(id)
+    val outcome = propagation(index)
     activate() // The constraint can be registered in L1
     outcome
   }
+}
+
+import oscar.cp.modeling._
+
+// Maybe we could harvest inequalities the same way, this could detect infeasibilities a < b < c < a
+// 
+
+object ImSlow extends CPModel with App {  // 2 minutes!
+  val tStart = System.currentTimeMillis()
+  val a = CPIntVar(0 until 2000000000)
+  val b = CPIntVar(0 until 2000000000 + 1)
+  val c = CPIntVar(0 until 2000000000 + 2)
+  
+  add(a < b)
+  add(b < c)
+  try {
+    add(c < a)
+  } catch { 
+    case e:Exception =>
+    val tEnd = System.currentTimeMillis()
+    println("Je prends " + (tEnd - tStart))
+  }
+  
 }
