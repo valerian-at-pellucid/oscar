bffef05fbf4b65fd0454da0dea6e05f7fad1d07f
StevenGay <devnull@localhost>
2014-07-15 15:02:41 +0200

    Scalar product last version

--- oscar-cp/src/main/scala/oscar/cp/constraints/ScalarProduct.scala
+++ oscar-cp/src/main/scala/oscar/cp/constraints/ScalarProduct.scala
@@ -16,13 +16,11 @@ object ScalarProduct {
   
   def zero(vars: Seq[CPIntVar], scalars: Seq[Int])(implicit cp: CPStore): Constraint = { // the scalar product must be zero
     assert(vars.size == scalars.size)
+//    sum(vars.zip(scalars).map { case (x, c) => x * c }) == 0
     
-    sum(vars.zip(scalars).map { case (x, c) => x * c }) == 0
-    
-    /*
     // step 1, take all indices where x_i * a_i is known in advance, and put these in a separate int
     val (constants, nonconstants) = vars.indices.partition { i => scalars(i) == 0 || vars(i).isBound }
-    val constant = constants.foldLeft(0){ (acc, i) => scalars(i) * vars(i).min }
+    val constant = constants.foldLeft(0){ (acc, i) => acc + scalars(i) * vars(i).min }
     
     // step 2, separate positive scalars from negative ones to avoid using an opposite view
     // Distributing them on either side of the equality sign 
@@ -44,47 +42,41 @@ object ScalarProduct {
       val negativesSum = if (!negatives.isEmpty) sum(negatives.map(i => vars(i) * -scalars(i))) else CPIntVar(0)
       (positivesSum + constant == negativesSum)
     }
-    * 
-    */
   }
   
   // the scalar product must be nonzero, code is a copypasta of zero
   def nonzero(vars: Seq[CPIntVar], scalars: Seq[Int])(implicit cp: CPStore): Constraint = {
     assert(vars.size == scalars.size)
+//    sum(vars.zip(scalars).map { case (x, c) => x * c }) != 0
     
-    sum(vars.zip(scalars).map { case (x, c) => x * c }) != 0
-    
-    /*
     val (constants, nonconstants) = vars.indices.partition { i => scalars(i) == 0 || vars(i).isBound }
-    val constant = constants.foldLeft(0){ (acc, i) => scalars(i) * vars(i).min }
+    val constant = constants.foldLeft(0){ (acc, i) => acc + scalars(i) * vars(i).min }
     
     val (positives, negatives) = nonconstants.partition(i => scalars(i) > 0)
     
     // TODO: are there optimizations for special cases?
-    val positivesSum = if (!positives.isEmpty) sum(positives.map(i => vars(i) *  scalars(i))) else CPIntVar(0)
-    val negativesSum = if (!negatives.isEmpty) sum(negatives.map(i => vars(i) * -scalars(i))) else CPIntVar(0)
-    (positivesSum + constant != negativesSum)
-    */
+    val positivesSum = if (positives.isEmpty) CPIntVar(0) else sum(positives.map(i => vars(i) *  scalars(i)))
+    val negativesSum = if (negatives.isEmpty) CPIntVar(0) else sum(negatives.map(i => vars(i) * -scalars(i)))
+    // TODO: find out why first version fails tests and not second
+//    (positivesSum + constant != negativesSum)
+    (positivesSum + constant - negativesSum != 0)
   }
   
   // the scalar product must be negative or zero, code is a copypasta of zero
   def leq(vars: Seq[CPIntVar], scalars: Seq[Int])(implicit cp: CPStore): Constraint = {
     assert(vars.size == scalars.size)
-
-    sum(vars.zip(scalars).map { case (x, c) => x * c }) <= 0
-
-    /*
+//    sum(vars.zip(scalars).map { case (x, c) => x * c }) <= 0
+    
     val (constants, nonconstants) = vars.indices.partition { i => scalars(i) == 0 || vars(i).isBound }
-    val constant = constants.foldLeft(0){ (acc, i) => scalars(i) * vars(i).min }
+    val constant = constants.foldLeft(0){ (acc, i) => acc + scalars(i) * vars(i).min }
     
     val (positives, negatives) = nonconstants.partition(i => scalars(i) > 0)
     
-    // TODO: special cases?
     val positivesSum = if (!positives.isEmpty) sum(positives.map(i => vars(i) *  scalars(i))) else CPIntVar(0)
     val negativesSum = if (!negatives.isEmpty) sum(negatives.map(i => vars(i) * -scalars(i))) else CPIntVar(0)
-    (positivesSum + constant <= negativesSum)
-    * 
-    */
+    // TODO: find out why first version is much more slow than the second
+//    (positivesSum + constant <= negativesSum)
+      (positivesSum + constant - negativesSum <= 0)
   }
 
 }
